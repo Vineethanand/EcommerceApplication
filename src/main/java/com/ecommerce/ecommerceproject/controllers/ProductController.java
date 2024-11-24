92b38bda8a23262ce45c98b8b770a554126bc694
Vineethanand <vineethanand.h@gmail.com>
2024-11-24 23:52:17 +0530

    Initial Commit

--- src/main/java/com/ecommerce/ecommerceproject/controllers/ProductController.java
+++ src/main/java/com/ecommerce/ecommerceproject/controllers/ProductController.java
@@ -1,10 +1,20 @@
 package com.ecommerce.ecommerceproject.controllers;
 
-import org.springframework.web.bind.annotation.Mapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.ecommerce.ecommerceproject.models.FakeStoreProduct;
+import com.ecommerce.ecommerceproject.services.ProductService;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/products")
 public class ProductController {
+
+    ProductService productService;
+
+    public ProductController(ProductService productService) {
+        this.productService = productService;
+    }
+    @GetMapping("/{id}")
+    public FakeStoreProduct getFakeStoreProductById(@PathVariable Long id) {
+        return productService.getFakeStoreProductById(id);
+    }
 }
