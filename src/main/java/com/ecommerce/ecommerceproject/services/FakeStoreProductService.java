6598a6ec3adbbcc9dd87911bf3668a1661e45158
Vineethanand <vineethanand.h@gmail.com>
2024-11-25 00:24:34 +0530

    Adding FakeStore product service - getProduct

--- src/main/java/com/ecommerce/ecommerceproject/services/FakeStoreProductService.java
+++ src/main/java/com/ecommerce/ecommerceproject/services/FakeStoreProductService.java
@@ -1,5 +1,7 @@
 package com.ecommerce.ecommerceproject.services;
 
+import com.ecommerce.ecommerceproject.dtos.FakeStoreProductDto;
+import com.ecommerce.ecommerceproject.models.FakeStoreCategory;
 import com.ecommerce.ecommerceproject.models.FakeStoreProduct;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
@@ -15,6 +17,25 @@ public class FakeStoreProductService implements ProductService{
 
     @Override
     public FakeStoreProduct getFakeStoreProductById(Long id) {
-        return null;
+
+        FakeStoreProductDto fakeStoreProductDto = restTemplate.getForObject("https://fakestoreapi.com/products/" + id, FakeStoreProductDto.class);
+        return convertFakeStoreProductDtoToProduct(fakeStoreProductDto);
+    }
+
+    private FakeStoreProduct convertFakeStoreProductDtoToProduct(FakeStoreProductDto fakeStoreProductDto) {
+        if (fakeStoreProductDto == null) {
+            return null; // Handle null gracefully
+        }
+
+        FakeStoreProduct product = new FakeStoreProduct();
+        product.setId(fakeStoreProductDto.getId());
+        product.setTitle(fakeStoreProductDto.getTitle());
+        product.setDescription(fakeStoreProductDto.getDescription());
+        product.setPrice(fakeStoreProductDto.getPrice());
+        FakeStoreCategory category = new FakeStoreCategory();
+        category.setTitle(fakeStoreProductDto.getCategory());
+        product.setCategory(category);
+
+        return product;
     }
 }
